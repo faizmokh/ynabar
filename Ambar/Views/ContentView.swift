1be7a946d61f1b11927e9f912e5feb7eac89fb9d
Faiz Mokhtar <mfaizmokhtar@airasia.com>
2020-05-27 21:52:03 +0800

    Prototype basic UI for the menu bar

--- Ambar/Views/ContentView.swift
+++ Ambar/Views/ContentView.swift
@@ -10,28 +10,83 @@ import SwiftUI
 import AppKit
 
 struct ContentView: View {
+
+    @State private var transactionAmount: String = ""
+    @State private var payee: String = ""
+    @State private var isMinus: Bool = false
+    private var transactionOptions = ["+", "-"]
+    private var payees = ["Transfer: RHB", "Transfer: Wallet", "Transfer: GrabPay"]
+    private var categories = ["Postpaid", "Parents", "Rent", "Internet", "Car Loans"]
+    private var repeatType = ["Never", "Daily", "Weekly", "Every other week", "Twice a month"]
+
+    @State private var selectedTransactionOption: String = ""
+    @State private var selectedPayee: String = ""
+    @State private var selectedCategory: String = ""
+    @State private var selectedAccount: String = ""
+    @State private var transactionDate: Date = Date()
+    @State private var selectedRepeatType: String = ""
+
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        return formatter
+    }
+
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            Text("Make\nEpic\nThings")
-                .font(Font.system(size: 34.0))
-                .fontWeight(.semibold)
-                .multilineTextAlignment(.leading)
-                .padding(.horizontal, 16.0)
-                .padding(.vertical, 12.0)
-                .frame(width: 360.0, height: 320.0, alignment: .topLeading)
-            Button(action: {
-                NSApplication.shared.terminate(self)
-            })
-            {
-                Text("Quit App")
-                .font(.caption)
-                .fontWeight(.semibold)
+        VStack(alignment: .leading, spacing: 5) {
+            Text("Add New Transaction")
+                .fontWeight(.medium)
+                .padding(.bottom, 5)
+            Form {
+                Section {
+                HStack {
+                    Picker(selection: $selectedTransactionOption, label: Text("Amount")) {
+                        ForEach(0..<transactionOptions.count) {
+                            Text(self.transactionOptions[$0])
+                        }
+                        .scaledToFit()
+                    }.pickerStyle(SegmentedPickerStyle())
+                    TextField("0.00", text: $transactionAmount)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                Picker(selection: $selectedPayee, label: Text("Payee")) {
+                    ForEach(0..<payees.count) {
+                        Text(self.payees[$0])
+                    }
+                }.pickerStyle(DefaultPickerStyle())
+                Picker(selection: $selectedCategory, label: Text("Category")) {
+                    ForEach(0..<categories.count) {
+                        Text(self.categories[$0])
+                    }
+                }.pickerStyle(DefaultPickerStyle())
+                Picker(selection: $selectedAccount, label: Text("Account")) {
+                    ForEach(0..<payees.count) { index in
+                        Text(self.payees[index])
+                        .tag(index)
+                    }
+                }.pickerStyle(DefaultPickerStyle())
+                DatePicker(selection: $transactionDate, in: ...Date(), displayedComponents: .date) {
+                    Text("Date")
+                }
+                .datePickerStyle(DefaultDatePickerStyle())
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                Picker(selection: $selectedRepeatType, label: Text("Repeat")) {
+                    ForEach(0..<repeatType.count) {
+                        Text(self.repeatType[$0])
+                    }
+                }.pickerStyle(DefaultPickerStyle())
+                }
+                Section {
+                Button(action: {
+                    NSApplication.shared.terminate(self)
+                }, label: {
+                    Text("Save Transaction")
+                }).buttonStyle(DefaultButtonStyle())
+                .foregroundColor(Color.primary)
+                }
             }
-            .padding(.trailing, 16.0)
-            .frame(width: 360.0, alignment: .trailing)
         }
-        .padding(0)
-        .frame(width: 360.0, height: 360.0, alignment: .top)
+        .padding()
     }
 }
 
